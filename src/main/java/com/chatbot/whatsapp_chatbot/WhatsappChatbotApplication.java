9a41c672968a952c6e8336ba7d63bf2438f2894b
Sahibdeep Singh <singhsahib110@gmail.com>
2026-03-26 15:21:26 +0530

    final fix-docker+code sync

--- src/main/java/com/chatbot/whatsapp_chatbot/WhatsappChatbotApplication.java
+++ src/main/java/com/chatbot/whatsapp_chatbot/WhatsappChatbotApplication.java
@@ -3,11 +3,10 @@ package com.chatbot.whatsapp_chatbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = "com.chatbot")
 public class WhatsappChatbotApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(WhatsappChatbotApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WhatsappChatbotApplication.class, args);
+    }
 }
